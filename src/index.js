b0feb49f03795344be282fb13c107c4126bd77ca
Leonid Shuliak <zeleny777@gmail.com>
2020-10-03 10:21:24 +0300

    innit:start reverse-int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    if(n<0){
+       n *=-1;
+       return Math.abs(n.toFixed(0).split('').reverse().join('')-0);
+    }else{
+    return Math.abs(n.toFixed(0).split('').reverse().join('')-0);}
+    }
